33d7d9b7da854f01121ca57992f25ac21bac2090
TheFetaxaFromNiN0 <43167122+TheFetaxaFromNiN0@users.noreply.github.com>
2018-09-11 13:18:34 +0300

    All new

---
+++ WindowsFormsApplication2/ExpressionChanges.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace WindowsFormsApplication2
+{
+    public class ExpressionChanges
+    {
+        //выводит false, если строка не соотвествует нужному формату
+        static public bool Check_string(string line)
+        {
+            if (line == "exp" || line == "Exp")
+            {
+                return false;
+            }
+            if (IsEmpty(line) | ChechNotX(line))
+                return false;
+            else
+                return true;
+            
+        }
+
+        static public bool IsEmpty(string line) // проверка на пустоту строки
+        {
+            if ((line == ""))
+                return true;
+            else
+                return false;
+        }
+
+        static private bool ChechNotX(string line) // проверка на содержание в строке символ "x"
+        {
+            if (!line.Contains("x"))
+                return true;
+            else
+                return false;
+        }
+    }
+}
